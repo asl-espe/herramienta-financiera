1bd3501edb852cb3f677c3370742ac783b9b8b2d
Eri10 <71108547+dayannna123@users.noreply.github.com>
2024-08-13 19:29:42 -0500

    Agrgar

--- gestion-financiera/src/App.jsx
+++ gestion-financiera/src/App.jsx
@@ -1,39 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Dashboard from '../public/components/Dashboard'
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Calculadora from './Calculadora';
+import Catalogo from './Catalogo';
+import Inicio from './Inicio';
+import Inventario from './Inventario';
+import Sidebar from './Sidebar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <Navbar />
-        <Dashboard />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Sidebar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/calculadora" element={<Calculadora />} />
+            <Route path="/inventario" element={<Inventario />} />
+            <Route path="/catalogo" element={<Catalogo />} />
+          </Routes>
+        </div>
       </div>
-
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
