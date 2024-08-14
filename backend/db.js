0cbdd4a2f9d7ad83d2949512c351af84a71e68ba
David Vargas <you@example.com>
2024-08-14 12:19:37 -0500

    Resueltos conflictos de merge entre calculadora y dev

--- backend/db.js
+++ backend/db.js
@@ -20,7 +20,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
->>>>>>> dev
+
 });
 
 connection.connect((err) => {
